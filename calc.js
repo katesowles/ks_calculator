b08f45b251c0e3a4f863b1fa10e383b984c60edb
Kate Sowles <katesowles@gmail.com>
2016-05-27 17:19:01 -0700

    the start of a calculator app with same color scheme as ks_calendar

---
+++ calc.js
@@ -0,0 +1,23 @@
+var calc = {};
+
+calc.add = function (a,b) {
+  return a + b;
+}
+
+calc.subtract = function (a,b) {
+  return a - b;
+}
+
+calc.multiply = function (a,b) {
+  return a * b;
+}
+
+calc.divide = function (a,b) {
+  return a / b;
+}
+
+var maths = [2, "+", 3];
+
+var test = maths[0] + maths[1] + maths [2];
+
+console.log(maths[0] + maths[1] + maths [2]);
